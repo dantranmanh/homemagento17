7a8598b4b8f8f39e71e82852bbb91e0496e31204
dantranmanh <24hnamdinh@gmail.com>
2015-02-01 22:20:54 +0700

    Add attribute fillter "xuatxu" and "cuahang" (dropdown list) for import

---
+++ app/code/local/BI/Extra/Model/Convert/Adapter/Product.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Catalog
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+
+class BI_Extra_Model_Convert_Adapter_Product
+    extends Mage_Catalog_Model_Convert_Adapter_Product
+{ 
+
+    /**
+     * Load product collection Id(s)
+     */
+    public function load()
+    {
+        $attrFilterArray = array();
+        $attrFilterArray ['name']           = 'like';       
+        $attrFilterArray ['sku']            = 'startsWith';
+        $attrFilterArray ['type']           = 'eq';
+        $attrFilterArray ['attribute_set']  = 'eq';
+        $attrFilterArray ['visibility']     = 'eq';
+        $attrFilterArray ['status']         = 'eq';
+        $attrFilterArray ['price']          = 'fromTo';
+        $attrFilterArray ['qty']            = 'fromTo';
+        $attrFilterArray ['store_id']       = 'eq';
+		 $attrFilterArray ['xuatxu']           = 'startsWith';
+		 $attrFilterArray ['cuahang']           = 'eq';
+		 
+        $attrToDb = array(
+            'type'          => 'type_id',
+            'attribute_set' => 'attribute_set_id'
+        );
+
+        $filters = $this->_parseVars();
+
+        if ($qty = $this->getFieldValue($filters, 'qty')) {
+            $qtyFrom = isset($qty['from']) ? (float) $qty['from'] : 0;
+            $qtyTo   = isset($qty['to']) ? (float) $qty['to'] : 0;
+
+            $qtyAttr = array();
+            $qtyAttr['alias']       = 'qty';
+            $qtyAttr['attribute']   = 'cataloginventory/stock_item';
+            $qtyAttr['field']       = 'qty';
+            $qtyAttr['bind']        = 'product_id=entity_id';
+            $qtyAttr['cond']        = "{{table}}.qty between '{$qtyFrom}' AND '{$qtyTo}'";
+            $qtyAttr['joinType']    = 'inner';
+
+            $this->setJoinField($qtyAttr);
+        }
+
+        parent::setFilter($attrFilterArray, $attrToDb);
+
+        if ($price = $this->getFieldValue($filters, 'price')) {
+            $this->_filter[] = array(
+                'attribute' => 'price',
+                'from'      => $price['from'],
+                'to'        => $price['to']
+            );
+            $this->setJoinAttr(array(
+                'alias'     => 'price',
+                'attribute' => 'catalog_product/price',
+                'bind'      => 'entity_id',
+                'joinType'  => 'LEFT'
+            ));
+        }
+
+        return parent::load();
+    }   
+}
